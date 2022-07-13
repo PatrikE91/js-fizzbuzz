5d04aba9814c854de550fc19b2ac6e308f3f2006
patrik <patrik.ellini@outlook.com>
2022-07-13 12:53:42 +0100

    fizzbuzz test completed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,23 @@
 // TODO: Write your function in this file
+function test(lower, upper) {
+  let numbers = []
 
+  for (let i = lower; i <= upper; i++) {
+    
+
+    if ((i) % 3 === 0 && (i) % 5 === 0) {
+      numbers.push('FizzBuzz')
+    } else if ((i) % 3 === 0) {
+      numbers.push('Fizz')
+    } else if ((i) % 5 === 0) {
+      numbers.push('Buzz')
+    } else {
+      numbers.push(i)
+    }
+  }
+  return numbers
+}
+
+console.log(test(0, 15))
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = test
